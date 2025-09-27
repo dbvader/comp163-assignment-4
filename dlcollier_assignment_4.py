5893e1bd315592ecf53df62b66e6fe9432b5a80a
dbvader <djwaun3@gmail.com>
2025-09-26 20:38:18 -0400

    git add dlcollier_assignment_4.py

--- dlcollier_assignment_4.py
+++ dlcollier_assignment_4.py
@@ -1,7 +1,7 @@
 student_name = "Dwaun Collier"  
-current_gpa = float(4.0)
+current_gpa = float(3.7)
 study_hours = 12
 social_points = 40
 stress_level = 100
 
-print(f"Here are your stats \n Student Name: {student_name} \n Current Gpa: {current_gpa} \n Study Hours: {study_hours} \n Social Points: {social_points} \n Stres level: {stress_level} ")
+print(f"Here are your stats \n Student Name: {student_name} \n Current Gpa: {current_gpa} \n Study Hours: {study_hours} \n Social Points: {social_points} \n Stress level: {stress_level} ")
