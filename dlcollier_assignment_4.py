6cd358c397084b26c6ff3e49579f9425ec2ec0de
dbvader <djwaun3@gmail.com>
2025-09-26 23:53:42 -0400

    git add dlcollier_assignment_4.py

--- dlcollier_assignment_4.py
+++ dlcollier_assignment_4.py
@@ -1,7 +1,12 @@
-student_name = "Dwaun Collier"  
-current_gpa = float(3.7)
+# step 1 setup
+student_name = "Dwaun Collier"
+current_gpa = 3.7
 study_hours = 12
 social_points = 40
 stress_level = 100
 
-print(f"Here are your stats \n Student Name: {student_name} \n Current Gpa: {current_gpa} \n Study Hours: {study_hours} \n Social Points: {social_points} \n Stress level: {stress_level} ")
+print(f"\n[BOOTING...] WELCOME {student_name} ... INITIALIZING STUDENT PROFILE...")
+print(f"CURRENT GPA: {current_gpa}")
+print(f"STUDY HOURS: {study_hours}")
+print(f"SOCIAL POINTS: {social_points}")
+print(f"STRESS LEVEL: {stress_level}\n")
